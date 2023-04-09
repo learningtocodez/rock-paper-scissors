b9499ab6ad14979a07a7e222e1b6c992300caf46
Learningtocode <106444354+learningtocodez@users.noreply.github.com>
2023-04-09 17:32:57 +1000

    changed some of the code around to suit

--- javascript.js
+++ javascript.js
@@ -1,62 +1,53 @@
 //Write a function that allows the computer to select from rock, paper, or scissors randomly;
 function getComputerChoice(){
     const select = ["rock", "paper", "scissors"];
-    return select [Math.floor (Math.random() *  select.length)];
+    return select [Math.floor (Math.random() * select.length)];
 }
 
 //write a game() function and call playRound inside to play 5 round game that can keep score
 //and report a winner and loser at the end. 
 //play 5 round game that keeps score and reports winner or loser at the end.
 
-let playScore = 0;  // keeps score, reports winner write out.
+let playScore = 0  // keeps score, reports winner write out.
 let computerScore = 0; // keeps score, reports winner write out.
 let draw = 0; //starts out as this, which it does currently.
-//const game = function
-
-function game() {
-    for (let i = 0; i < 5; i++); {
-    	playRound(playerSelection, computerSelection);
-        
-    } 
-}
-    
+//const game = function   
 
 //console.log((playScore,computerScore));
 //console.log(playScore);
 //console.log(computerScore);
 
+//Player selection string "rock, paper, scissors";
+const playerSelection = prompt("rock, paper, scissors");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 //Write a function that plays a single round of rock paper scissors;
 //The function should take two parameters, playerSelection and computerSelection;
 //return a string that declares the winner of the round example: "You Lose! computerSelection beats playersSelection";
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `draw ${playerSelection}`;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playScore += 1;
-        console.log(playScore);
-        return `Winner rock beats scissors`;
+function playRound(playerSelection,computerSelection) {
+      if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `Winner rock beats scissors` && ++playScore;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playScore += 1;
-        console.log(playScore);
-        return `Winner paper beats rock`;
+        return `Winner paper beats rock` && ++playScore;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playScore += 1;
-        console.log(playScore);
-        return `Winner scissors beat paper`;
-    
-    } else if  (computerScore += 1);  {
-        console.log(computerScore);
-    } return`You lose! ${computerSelection} beats ${playerSelection}`;
-     
+        return `Winner scissors beat paper` && ++playScore;
+    } else if (playerSelection === computerSelection) {
+        return `draw ${playerSelection} && ${computerSelection}`;
+    } else { return`You lose! ${computerSelection} beats ${playerSelection}` && ++computerScore
+}    
 }
-//Player selection string "rock, paper, scissors";
-const playerSelection = prompt("rock, paper, scissors");
-const computerSelection = getComputerChoice();
-//console.log(playRound(playerSelection, computerSelection));
-game(playerSelection, computerSelection);{
-   playScore += 1;
-   computerScore += 1;
+function game() {
+    for (let i = 0; i < 5; i++); {
+     playRound(playerSelection, computerSelection);
+       
+    } 
 }
-console.log('playScore = ' + playScore);
-console.log('computerScore = ' + computerScore);
+console.log(++playScore);
+console.log(++computerScore);
+
+game();{
+   ++playScore;
+   ++computerScore;
+}
+   
